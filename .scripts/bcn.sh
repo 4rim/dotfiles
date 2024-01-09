efbd90dc2f9548735059abf082d35a8dcaaca086
Arim Lim <arimlim13@gmail.com>
2024-01-08 20:44:11 -0500

    Added cancel option to bcn.sh

--- .scripts/bcn.sh
+++ .scripts/bcn.sh
@@ -8,7 +8,8 @@ if [ $num_devices -eq 0 ]; then
 elif [ $num_devices -eq 1 ]; then
     MAC=$(bluetoothctl devices | awk '{ print $2 }')
 else
-    select=$(bluetoothctl devices | awk '{ print $3 }' | dmenu -l $num_devices)
+    select=$(bluetoothctl devices | awk '{ print $3 }' | sed -e "$num_devices a Cancel" | dmenu -l $(($num_devices+1)))
+    [ $select = "Cancel" ] && exit 0
     MAC=$(bluetoothctl devices | awk '/'"$select"'/ { print $2 }')
 fi
 
@@ -16,9 +17,9 @@ is_connected=$(bluetoothctl info $MAC | awk '/Connected:/ { print $2 }')
 
 if [ $is_connected = "no" ]; then
     dunstify -h $tag -u normal "Attempting to connect to $select".
-    bluetoothctl connect $MAC; bat=$(bluetoothctl info $MAC | awk '/Battery Percentage:/ { print substr($4,2,length($4)-2) }')
+    bluetoothctl connect $MAC
     dunstify -h $tag -u normal "Connected to $select!" || dunstify -h $tag -u critical "Failed to connect to $select".
 else
     dunstify -h $tag -u normal "Attempting to disconnect from $select"
-    (bluetoothctl disconnect $MAC && dunstify -h $tag -u normal "Disconnected from $select successfully. (Battery: $bat)") || dunstify -h $tag -u critical "Failed to disconnect from $select."
+    (bluetoothctl disconnect $MAC && dunstify -h $tag -u normal "Disconnected from $select successfully.") || dunstify -h $tag -u critical "Failed to disconnect from $select."
 fi
